497f04d7104455b811bdaf4ceb455c7b8cb39e40
Nicholas Berlette <nick@berlette.com>
2024-03-20 15:37:31 -0700

    fix: remove global type augmentation for JSR

--- symbol.ts
+++ symbol.ts
@@ -1,37 +1,48 @@
 /// <reference lib="es2015.symbol" />
 
-declare global {
-  interface SymbolConstructor {
-    /**
-     * A method that is used to release resources held by an object. Called by
-     * the semantics of the `using` statement.
-     */
-    readonly dispose: unique symbol;
+export interface SymbolConstructor
+  extends Omit<typeof globalThis.Symbol, "dispose" | "asyncDispose"> {
+  /**
+   * A method that is used to release resources held by an object. Called by
+   * the semantics of the `using` statement.
+   */
+  readonly dispose: unique symbol;
 
-    /**
-     * A method that is used to asynchronously release resources held by an
-     * object. Called by the semantics of the `await using` statement.
-     */
-    readonly asyncDispose: unique symbol;
-  }
+  /**
+   * A method that is used to asynchronously release resources held by an
+   * object. Called by the semantics of the `await using` statement.
+   */
+  readonly asyncDispose: unique symbol;
 }
 
-if (typeof Symbol === "function" && typeof Symbol.dispose !== "symbol") {
-  Object.defineProperty(globalThis.Symbol, "dispose", {
-    value: Symbol("Symbol.dispose"),
+export const Symbol: SymbolConstructor = (globalThis.Symbol ??
+  ((description: string) => ({
+    description,
+    toString: () => `Symbol(${description})`,
+  }))) as unknown as SymbolConstructor;
+
+if (
+  typeof globalThis.Symbol === "function" &&
+  typeof Symbol.dispose !== "symbol"
+) {
+  Reflect.defineProperty(Symbol, "dispose", {
+    // deno-lint-ignore no-explicit-any
+    value: (Symbol as any)("Symbol.dispose"),
     enumerable: false,
     configurable: false,
     writable: false,
   });
 }
 
-if (typeof Symbol === "function" && typeof Symbol.asyncDispose !== "symbol") {
-  Object.defineProperty(globalThis.Symbol, "asyncDispose", {
-    value: Symbol("Symbol.asyncDispose"),
+if (
+  typeof globalThis.Symbol === "function" &&
+  typeof Symbol.asyncDispose !== "symbol"
+) {
+  Reflect.defineProperty(Symbol, "asyncDispose", {
+    // deno-lint-ignore no-explicit-any
+    value: (Symbol as any)("Symbol.asyncDispose"),
     enumerable: false,
     configurable: false,
     writable: false,
   });
 }
-
-export {};
